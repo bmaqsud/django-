528994da36b86cc1891608c6ec7a9d41c00e95a2
Maqsud Boltayev <maqsud@Maqsuds-MacBook-Air.local>
2025-10-18 19:56:55 +0500

    homepage

---
+++ todo_project/todo_project/settings.py
@@ -0,0 +1,11 @@
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'todos',  # 🔥 bizning app
+]
+DEBUG = True
+ALLOWED_HOSTS = []
